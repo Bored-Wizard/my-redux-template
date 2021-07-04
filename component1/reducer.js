7778bb6ea848e944fbdb98c1f953761eb6f24df0
“Bored-Alchemist” <sujanbarman12345q@gmail.com>
2021-07-04 12:09:21 +0530

    fixed

--- component1/reducer.js
+++ component1/reducer.js
@@ -1,17 +1,17 @@
 import { actionType1 } from './types';
 
 const initialValue = {
+    date: "Select Date"
 }
 
-export const reducer = (state = initialValue, action) => {
+export const component1reducer = (state = initialValue, action) => {
     switch(action.type){
         case actionType1: return {
             ...initialValue,
-
             ...action.payload
         }
         default: return{
-            state
+            ...state
         }
     }
 }
